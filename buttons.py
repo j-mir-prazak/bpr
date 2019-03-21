c0f336a79d66e6cd11c403ccd871ce74276dcd16
j-mir-prazak <j.mir.prazak@gmail.com>
2019-03-21 19:24:06 +0100

    quick git

--- buttons.py
+++ buttons.py
@@ -2,6 +2,19 @@
 from random import randint
 from time import sleep
 import sys
+from gpiozero import Button
+import RPi.GPIO as GPIO
+
+
+b0 = 4
+b1 = 17
+b2 = 18
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(b0,GPIO.IN)
+GPIO.setup(b1,GPIO.IN)
+GPIO.setup(b2,GPIO.IN)
 
 
 
@@ -16,20 +29,15 @@ button0 = 0
 button1 = 0
 button2 = 0
 
-
 while 1:
 
-    irand0 = randint(0,999)
-    irand1 = randint(0,999)
-    irand2 = randint(0,999)
-
-    if irand0 > 690:
+    if GPIO.input(b0):
         button0 = 1
 
-    if irand1 > 990:
+    if GPIO.input(b1):
         button1 = 1
 
-    if irand2 > 690:
+    if GPIO.input(b2):
         button2 = 1
 
     if (button0 + button1 + button2) > 0:
@@ -40,3 +48,30 @@ while 1:
     button1 = 0
     button2 = 0
     sleep(0.01)
+
+
+
+
+# while 1:
+#
+#     irand0 = randint(0,999)
+#     irand1 = randint(0,999)
+#     irand2 = randint(0,999)
+#
+#     if irand0 > 690:
+#         button0 = 1
+#
+#     if irand1 > 990:
+#         button1 = 1
+#
+#     if irand2 > 690:
+#         button2 = 1
+#
+#     if (button0 + button1 + button2) > 0:
+#         print("buttons:" + str(button0) + ":" + str(button1) + ":" + str(button2) )
+#         sys.stdout.flush()
+#
+#     button0 = 0
+#     button1 = 0
+#     button2 = 0
+#     sleep(0.01)

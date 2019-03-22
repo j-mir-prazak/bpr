5eaf76943c7483a4ed0941cb7a32e7bfa428edb0
j-mir-prazak <j.mir.prazak@gmail.com>
2019-03-22 20:16:35 +0100

    quick git

--- omxplayer.js
+++ omxplayer.js
@@ -2,7 +2,7 @@ let spawn = require('child_process').spawn;
 let EventEmitter = require('events');
 let StringDecoder = require('string_decoder').StringDecoder
 
-let decoder = new StringDecoder('utf-8')
+var decoder = new StringDecoder('utf-8')
 
 let args = new Array(
 
@@ -17,15 +17,28 @@ let args = new Array(
 let omxProcess = spawn('/usr/bin/omxplayer.bin', args, {detached: true});
 
 omxProcess.stdout.on('data', (data) => {
-	var decoder = new StringDecoder('utf-8')
+	// var decoder = new StringDecoder('utf-8')
 	var string = decoder.write(data)
 	string=string.split(/\r?\n/)
 	for( var i = 0; i < string.length; i++) {
-		console.log(string[i])
-		if ( string[i].match(/Current volume:/ )) {
-			var vol = string[i].replace(/Current volume: (.*)dB/i,"$1")
-			vol = parseFloat(vol)
-			console.log(vol)
+		// console.log(string[i])
+		// if ( string[i].match(/Current volume:/ )) {
+		// 	var vol = string[i].replace(/Current volume: (.*)dB/i,"$1")
+		// 	vol = parseFloat(vol)
+		// 	console.log(vol)
+		}
+	}
+});
+omxProcess.stderr.on('data', (data) => {
+	// var decoder = new StringDecoder('utf-8')
+	var string = decoder.write(data)
+	string=string.split(/\r?\n/)
+	for( var i = 0; i < string.length; i++) {
+		// console.log(string[i])
+		// if ( string[i].match(/Current volume:/ )) {
+		// 	var vol = string[i].replace(/Current volume: (.*)dB/i,"$1")
+		// 	vol = parseFloat(vol)
+		// 	console.log(vol)
 		}
 	}
 });

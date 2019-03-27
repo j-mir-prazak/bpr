aab99dc60b1406202dd0b2a144592944ef99f2dd
j-mir-prazak <j.mir.prazak@gmail.com>
2019-03-27 17:46:44 +0100

    quick git

--- connection_check.sh
+++ connection_check.sh
@@ -4,7 +4,7 @@ echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
     echo "online"
-    exit 1
+    exit 0
 else
     echo "offline"
     exit 1
